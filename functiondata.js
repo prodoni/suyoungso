e6f7a598a7e138fa095dab5a160c418e9d04a074
prodoni <105639150+prodoni@users.noreply.github.com>
2022-08-14 17:42:01 +0900

    Add files via upload

---
+++ functiondata.js
@@ -0,0 +1,15 @@
+function list(){
+    document.write(`<ol>
+        <li><a href="main.html">main</a></li>
+        <li><a href="1.html">ㅆㅅㅌㅊ</a></li>
+        <li><a href="2.html">쌍괄</a></li>
+        <li><a href="3.html">해정</a></li>
+        <li><a href="4.html">견생</a></li>
+        <li><a href="5.html">타락천사</a></li>
+        <li><a href="6.html">릴리</a></li>
+        <li><a href="7.html">창녕</a></li>
+        <li><a href="8.html">나피</a></li>
+        <li><a href="9.html">고먐미</a></li>
+        </ol>
+    `)
+}
